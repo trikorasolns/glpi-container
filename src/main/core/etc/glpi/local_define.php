527e5e6b38584046bdec7ec798a3e6cb057eef57
Antonio Costa <a.costa@trikorasolutions.com>
2023-09-04 00:09:36 +0200

    fix: required volume folders

--- src/main/core/etc/glpi/local_define.php
+++ src/main/core/etc/glpi/local_define.php
@@ -1,16 +1,70 @@
 <?php
 define('GLPI_FILES_DIR', '/var/lib/glpi');
-define('GLPI_DOC_DIR',        GLPI_FILES_DIR);
-define('GLPI_CRON_DIR',       GLPI_FILES_DIR . '/_cron');
-define('GLPI_DUMP_DIR',       GLPI_FILES_DIR . '/_dumps');
-define('GLPI_GRAPH_DIR',      GLPI_FILES_DIR . '/_graphs');
-define('GLPI_LOCK_DIR',       GLPI_FILES_DIR . '/_lock');
-define('GLPI_PICTURE_DIR',    GLPI_FILES_DIR . '/_pictures');
-define('GLPI_PLUGIN_DOC_DIR', GLPI_FILES_DIR . '/_plugins');
-define('GLPI_RSS_DIR',        GLPI_FILES_DIR . '/_rss');
-define('GLPI_SESSION_DIR',    GLPI_FILES_DIR . '/_sessions');
-define('GLPI_TMP_DIR',        GLPI_FILES_DIR . '/_tmp');
-define('GLPI_UPLOAD_DIR',     GLPI_FILES_DIR . '/_uploads');
-define('GLPI_CACHE_DIR',      GLPI_FILES_DIR . '/_cache');
+define('GLPI_VAR_DIR',          GLPI_FILES_DIR);
+define('GLPI_DOC_DIR',          GLPI_FILES_DIR);
+define('GLPI_CACHE_DIR',        GLPI_FILES_DIR . '/_cache');
+define('GLPI_CRON_DIR',         GLPI_FILES_DIR . '/_cron');
+define('GLPI_DUMP_DIR',         GLPI_FILES_DIR . '/_dumps');
+define('GLPI_GRAPH_DIR',        GLPI_FILES_DIR . '/_graphs');
+define('GLPI_LOCAL_I18N_DIR',   GLPI_FILES_DIR . '/_locales');
+define('GLPI_LOCK_DIR',         GLPI_FILES_DIR . '/_lock');
+define('GLPI_PICTURE_DIR',      GLPI_FILES_DIR . '/_pictures');
+define('GLPI_PLUGIN_DOC_DIR',   GLPI_FILES_DIR . '/_plugins');
+define('GLPI_RSS_DIR',          GLPI_FILES_DIR . '/_rss');
+define('GLPI_SESSION_DIR',      GLPI_FILES_DIR . '/_sessions');
+define('GLPI_TMP_DIR',          GLPI_FILES_DIR . '/_tmp');
+define('GLPI_UPLOAD_DIR',       GLPI_FILES_DIR . '/_uploads');
+define('GLPI_UPLOAD_DIR',       GLPI_FILES_DIR . '/_uploads');
+define('GLPI_INVENTORY_DIR',    GLPI_FILES_DIR . '/_inventories');
+define('GLPI_MARKETPLACE_DIR',  GLPI_FILES_DIR . '/marketplace');
 
 define('GLPI_LOG_DIR', '/var/log/glpi');
+
+// GLPI_ROOT: "/var/www/glpi"
+// GLPI_CONFIG_DIR: "/var/www/glpi/config"
+// GLPI_VAR_DIR: "/var/www/glpi/files"
+// GLPI_MARKETPLACE_DIR: "/var/www/glpi/marketplace"
+// GLPI_USE_CSRF_CHECK: "1"
+// GLPI_CSRF_EXPIRES: "7200"
+// GLPI_CSRF_MAX_TOKENS: "100"
+// GLPI_USE_IDOR_CHECK: "1"
+// GLPI_IDOR_EXPIRES: "7200"
+// GLPI_ALLOW_IFRAME_IN_RICH_TEXT: false
+// GLPI_SERVERSIDE_URL_ALLOWLIST: ["/^(https?|feed):\\/\\/[^@:]+(\\/.*)?$/"]
+// GLPI_TELEMETRY_URI: "https://telemetry.glpi-project.org"
+// GLPI_INSTALL_MODE: "TARBALL"
+// GLPI_NETWORK_MAIL: "glpi@teclib.com"
+// GLPI_NETWORK_SERVICES: "https://services.glpi-network.com"
+// GLPI_MARKETPLACE_ALLOW_OVERRIDE: true
+// GLPI_MARKETPLACE_MANUAL_DOWNLOADS: true
+// GLPI_USER_AGENT_EXTRA_COMMENTS: ""
+// GLPI_DISABLE_ONLY_FULL_GROUP_BY_SQL_MODE: "1"
+// GLPI_AJAX_DASHBOARD: "1"
+// GLPI_CALDAV_IMPORT_STATE: 0
+// GLPI_DEMO_MODE: "0"
+// GLPI_CENTRAL_WARNINGS: "1"
+// GLPI_DOC_DIR: "/var/www/glpi/files"
+// GLPI_CACHE_DIR: "/var/www/glpi/files/_cache"
+// GLPI_CRON_DIR: "/var/www/glpi/files/_cron"
+// GLPI_DUMP_DIR: "/var/www/glpi/files/_dumps"
+// GLPI_GRAPH_DIR: "/var/www/glpi/files/_graphs"
+// GLPI_LOCAL_I18N_DIR: "/var/www/glpi/files/_locales"
+// GLPI_LOCK_DIR: "/var/www/glpi/files/_lock"
+// GLPI_LOG_DIR: "/var/www/glpi/files/_log"
+// GLPI_PICTURE_DIR: "/var/www/glpi/files/_pictures"
+// GLPI_PLUGIN_DOC_DIR: "/var/www/glpi/files/_plugins"
+// GLPI_RSS_DIR: "/var/www/glpi/files/_rss"
+// GLPI_SESSION_DIR: "/var/www/glpi/files/_sessions"
+// GLPI_TMP_DIR: "/var/www/glpi/files/_tmp"
+// GLPI_UPLOAD_DIR: "/var/www/glpi/files/_uploads"
+// GLPI_INVENTORY_DIR: "/var/www/glpi/files/_inventories"
+// GLPI_NETWORK_REGISTRATION_API_URL: "https://services.glpi-network.com/api/registration/"
+// GLPI_MARKETPLACE_PLUGINS_API_URI: "https://services.glpi-network.com/api/glpi-plugins/"
+// GLPI_I18N_DIR: "/var/www/glpi/locales"
+// GLPI_VERSION: "10.0.6"
+// GLPI_SCHEMA_VERSION: "10.0.6"
+// GLPI_MARKETPLACE_PRERELEASES: false
+// GLPI_MIN_PHP: "7.4.0"
+// GLPI_MAX_PHP: "8.3.0"
+// GLPI_YEAR: "2023"
+
